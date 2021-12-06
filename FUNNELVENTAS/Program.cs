2b8fa2ffcd631620e6141a80ee9ba819ecee45a2
Tito Arevalo <titotono00@hotmail.es>
2021-12-05 18:02:10 -0600

    Final

--- FUNNELVENTAS/Program.cs
+++ FUNNELVENTAS/Program.cs
@@ -16,7 +16,7 @@ namespace FUNNELVENTAS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new frmLogin());
         }
     }
 }
